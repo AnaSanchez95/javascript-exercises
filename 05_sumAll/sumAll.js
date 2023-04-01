9e41c919c0545ca0a64c163253f3ac9344d46062
AnaSanchez95 <120680319+AnaSanchez95@users.noreply.github.com>
2023-04-01 16:40:17 +0200

    Do exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,25 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = 0;
+      
+    if (num1 < 0 || num2 < 0) {
+        return "ERROR";
+    }
+
+    else if (num1 <= num2) {
+        
+    }
+
+    else if (num2 > num1) {
+        const aux = num1;
+        num2 = num1;
+        num1 = aux; 
+    }
+    
+    for (i = num1; i <= num2; i++){
+        sum += i;
+    }
+    return sum;
+    
 
 };
 
