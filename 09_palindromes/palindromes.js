d0a1fc0ab64f0a717094ff0db259ec8eb4df7146
AnaSanchez95 <120680319+AnaSanchez95@users.noreply.github.com>
2023-04-06 16:56:42 +0200

    Do exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let newWord = '';
+    for (let i = string.length ; i >= 0; i--) {        
+        newWord += string.charAt(i);
+    }
+    if (string === newWord) {
+        return true;
+    }
 };
 
 // Do not edit below this line
