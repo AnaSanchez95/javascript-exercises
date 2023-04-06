c819a0c98dbef1064949908dd07d30351bb85b9c
AnaSanchez95 <120680319+AnaSanchez95@users.noreply.github.com>
2023-04-06 18:02:30 +0200

    Do exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(a) {
+let n1 = 1;
+let n2 = 0;
+let aux = 0;
+let i = 1;
+const fibonacciSequence = {};
 
+while (i <= a) {        
+    fibonacciSequence[i] = n1;
+    aux = n1;
+    n1 += n2;
+    n2 = aux;
+    i++;
+}
+if (a <= 0) {
+    return 'OOPS';
+}
+else {
+    return fibonacciSequence[a];
+}
 };
 
 // Do not edit below this line
