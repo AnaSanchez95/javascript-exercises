bc61dd6e2050e402e5c53b7596dc5002ddf55bf7
AnaSanchez95 <120680319+AnaSanchez95@users.noreply.github.com>
2023-04-06 16:42:19 +0200

    Do exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,28 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((a, b) => a + b, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+ return array.reduce((a, b) => a * b, 0);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let total = 1;
+  for (i = 1; i <= a; i++){
+    total = a * i;
+  };
 };
 
 // Do not edit below this line
